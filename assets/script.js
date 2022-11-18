d7e095f7e728fea9a41cf1022864ea19a2cc452c
Mariam-asiimwe <m.asiimwe@outlook.com>
2022-11-18 18:37:00 +0000

    update to js script

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,13 @@
-var topics = ["HTML" , "CSS", "Git", "Javascript"];
-
-for (var x = 0; x <topics.length; x++) {
+  var topics = ["HTML , CSS, Git, Javascript"];
+var randomTopic = topics[Math.fllor(Math.random() * topics.lentght)];
+  function listTopics() {
+    for (var x = 0; x <topics.length; x++) {
     console.log(topics[x]);
+  }
 }
 
-/*
+
+var topic = "HTML";
 if (topic === 'HTML') {
   console.log("Let's study HTML!");
 } else if (topic === 'CSS') {
@@ -17,8 +20,18 @@ if (topic === 'HTML') {
   console.log('Please try again!');
 }
 
-var topics = ["HTML", "CSS", "Git", "Javascript"];
+}
+
+var topic = ["HTML", "CSS", "Git", "Javascript"];
 for(var x = 0; x < shapes.length; x++) {
 console.log(shapes[x]);
 }
-*/
+
+console.log('Here are the topics we learned through Prework:');
+listTopics();
+console.log('Which topic should we study first?');
+selectTopic();
+
+listTopic();
+
+selectTopic();
